b76a51d2742322247a16579892d96b94465f736c
DavidDionise <david.dionise@gmail.com>
2022-11-03 15:03:51 -0400

    fixed compass bugs

--- src/core/compass/compass_publisher.py
+++ src/core/compass/compass_publisher.py
@@ -1,11 +1,12 @@
 
 
+import time
 from core.publisher import Publisher
 import smbus2 as smbus
 import logging
 
 
-class CompassController(Publisher):
+class CompassPublisher(Publisher):
 
     def __init__(self, i2c_bus=1, i2c_address=0x70, rate=0.2):
         """
@@ -17,24 +18,30 @@ class CompassController(Publisher):
         :type i2c_address: 
         :param rate: Rate in seconds of new compass readings (default 0.2)
         """
+
+        super().__init__()
+
         self._i2c_bus = i2c_bus
         self._i2c_address = i2c_address
         self._rate = rate
 
-        self._smbus = smbus.SMBus(i2c_address)
-
-    def bytes_to_degrees(b1, b2):
-        return ((b1[0] << 8) + b2[0]) / 10
+        self._smbus = smbus.SMBus(i2c_bus)
 
     def run(self):
         while True:
-            write = smbus.i2c_msg.write(self._address, [0x00, 0x31])
+            write = smbus.i2c_msg.write(self._i2c_address, [0x00, 0x31])
             read = smbus.i2c_msg.read(self._i2c_address, 8)
 
             self._smbus.i2c_rdwr(write, read)
 
-            high_bits, low_bits = read[1:3]
-            degress = self.bytes_to_degrees(high_bits, low_bits)
+            high_bits, low_bits = read.buf[1:3]
+            degress = _bytes_to_degrees(high_bits, low_bits)
 
             logging.debug("Compass Degrees: ", degress)
             self.publish(degress)
+
+            time.Sleep(self._rate)
+
+
+def _bytes_to_degrees(b1, b2):
+    return ((b1 << 8) + b2) / 10
